b630eb9818f68401f7147c70dc016853a168a6f1
varshadarji <varshadarji222@gmail.com>
2020-08-19 13:53:53 +0530

    my first commit reactweb

---
+++ build/precache-manifest.cd1e1e6bc427f63f267789eb3892b594.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62d099e64c6c442a5c913f4fc72e02f",
+    "url": "/React-live-website/index.html"
+  },
+  {
+    "revision": "1295975bde3523471a26",
+    "url": "/React-live-website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "40205e2989564a729de8",
+    "url": "/React-live-website/static/css/main.72dc4bfe.chunk.css"
+  },
+  {
+    "revision": "1295975bde3523471a26",
+    "url": "/React-live-website/static/js/2.ac3f56cc.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/React-live-website/static/js/2.ac3f56cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40205e2989564a729de8",
+    "url": "/React-live-website/static/js/main.8f9c40fc.chunk.js"
+  },
+  {
+    "revision": "1331a6527715da7f90a7",
+    "url": "/React-live-website/static/js/runtime-main.ccd97a8c.js"
+  },
+  {
+    "revision": "0cd447b56fa55133e8fe3fe86c8091e1",
+    "url": "/React-live-website/static/media/getstarted.0cd447b5.jpg"
+  }
+]);
